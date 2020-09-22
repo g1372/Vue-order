68edc90a25c18a18e404e7b9614714c95dba6c8c
g1372 <1372933839@qq.com>
2020-09-22 20:28:47 +0800

    first commit

---
+++ src/components/FooterNav/FooterNavss.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <link rel="stylesheet" href="//at.alicdn.com/t/font_2085780_ncj58pfpkrc.css" />
+
+    <!-- 上面的内容 -->
+    <router-view></router-view>
+    
+    <!-- 底部导航 -->
+    <footer class="footer_guide border-1px">
+      <router-link to="/msite" class="guide_item" :class="{on:$route.path=='/msite'}">
+        <span class="item_icon">
+          <i class="iconfont icon-iconfont27"></i>
+        </span>
+        <span>外卖</span>
+      </router-link>
+      <router-link to="/search" class="guide_item" :class="{on:$route.path=='/search'}">
+        <span class="item_icon">
+          <i class="iconfont icon-iconfontsearch"></i>
+        </span>
+        <span>搜索</span>
+      </router-link>
+      <router-link to="/order" class="guide_item" :class="{on:$route.path=='/order'}">
+        <span class="item_icon">
+          <i class="iconfont icon-iconfontlou"></i>
+        </span>
+        <span>订单</span>
+      </router-link>
+      <router-link to="/profile" class="guide_item" :class="{on:$route.path=='/profile'}">
+        <span class="item_icon">
+          <i class="iconfont icon-iconfonticon-yonghu"></i>
+        </span>
+        <span>我的</span>
+      </router-link>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="stylus">
+@import '../../assets/stylus/mixins.styl';
+
+.footer_guide { // footer
+  top-border-1px(#e4e4e4);
+  position: fixed;
+  z-index: 100;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #fff;
+  width: 100%;
+  height: 50px;
+  display: flex;
+
+  .guide_item {
+    display: flex;
+    flex: 1;
+    text-align: center;
+    flex-direction: column;
+    align-items: center;
+    margin: 5px;
+    color: #999999;
+    text-decoration: none;
+
+    &.on {
+      color: $dColor;
+    }
+
+    span {
+      font-size: 12px;
+      margin-top: 4px;
+      margin-bottom: 2px;
+
+      .iconfont {
+        font-size: 18px;
+      }
+    }
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,33 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Msite from "../views/Msite/Msite.vue";
+import Order from "../views/Order/Order.vue";
+import Profile from "../views/Profile/Profile.vue";
+import Search from "../views/Search/Search.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/msite",
+    component: Msite,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/order",
+    component: Order,
+  },
+  {
+    path: "/profile",
+    component: Profile,
+  },
+  {
+    path: "/search",
+    component: Search,
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
