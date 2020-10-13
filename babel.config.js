74d92238de0efb86b919620dd71070251e0e5fda
“用g1204 <1372933839@qq.com>
2020-10-13 11:56:17 +0800

    456

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,5 @@
 module.exports = {
   presets: [
-    "@vue/cli-plugin-babel/preset",
-    "transform-runtime",
-    [
-      "component",
-      [
-        {
-          libraryName: "mint-ui",
-          style: true,
-        },
-      ],
-    ],
-  ],
-};
+    '@vue/cli-plugin-babel/preset'
+  ]
+}
